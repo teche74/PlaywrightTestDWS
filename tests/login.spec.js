dc4d790ccb00a84b57fcc729a2bca771b9a05379
Ujjwal Bisht <ujjwalbisht55@gmail.com>
2026-03-31 12:58:37 +0530

    Update login.spec.js

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,17 @@
-import {test , expect} from "@playwright/test"
+import { test, expect } from "@playwright/test";
+
+test('Demo Web Shop test' , async({ page }) =>{
+    await page.goto("https://demowebshop.tricentis.com/");
+    
+    await page.click('a[href ="/login"]');
+
+    await page.fill('#Email' , "tempraman54@gmail.com");
+
+    await page.fill('#Password' , "23456789dfghjkl");
 
 
-test('Checking Login Functionality' , async({page})=>{
-  page.goto("")
+    await page.click('input[value="Log in"]');
+
+
+    await expect(page).toHaveURL("https://demowebshop.tricentis.com/login");
 })
